11ad67e12c4ae478ee04371bc8882a30f397f428
A.Bond <alexey.bond.94.55@gmail.com>
2024-08-18 12:34:19 +0600

    intro; minor improvements

--- tile_select_button.gd
+++ tile_select_button.gd
@@ -4,3 +4,16 @@ class_name TileSelectButton
 
 @export
 var meta_name: String
+
+@export
+var minus_meta: Array[String] = []
+
+func get_domain(patcher: WFC2DPatcher) -> WFCBitSet:
+	var domain := patcher.get_domain_by_meta(meta_name, true)
+
+	for mm in minus_meta:
+		domain.intersect_in_place(
+			patcher.get_domain_by_meta(mm, true).invert()
+		)
+
+	return domain
